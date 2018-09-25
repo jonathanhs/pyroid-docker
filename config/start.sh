392096df8f5175aa154833b5d3e0fc3e4fbe85dc
Jonathan Hadisuryo <jonathan.hadisuryo@gmail.com>
2018-09-24 17:17:11 -0700

    dockerfile command script

--- config/start.sh
+++ config/start.sh
@@ -4,11 +4,11 @@ HOME=/root
 
 # activate virtualenv and start gunicorn wsgi
 if [ $1 = django ]; then
-    cd $HOME/django_app && pipenv shell
-    gunicorn app.wsgi -w 4 -b 0.0.0.0:8000 --chdir=app --log-file - --daemon
+    cd $HOME/django_app
+    pipenv run gunicorn app.wsgi -w 4 -b 0.0.0.0:8000 --chdir=app --log-file - --daemon
 elif [ $1 = flask ]; then
-    cd $HOME/flask_app && pipenv shell && cd app
-    gunicorn -w 4 -b 0.0.0.0:8000 wsgi --log-file - --daemon
+    cd $HOME/flask_app
+    pipenv run gunicorn app.wsgi -w 4 -b 0.0.0.0:8000 --chdir=app --log-file - --daemon
 fi
 
 # start nginx server
